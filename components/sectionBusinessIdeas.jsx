237f08ccfbec2271499e8efee33480d9c41437fc
nataliamachella <nataliamachella@gmail.com>
2023-05-21 16:58:39 -0300

    Agregué estilos

--- components/sectionBusinessIdeas.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function sectionBusinessIdeas() {
-  return <div>sectionBusinessIdeas</div>;
-}
--- components/sectionRecipes.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function sectionRecipes() {
-  return <div>sectionRecipes</div>;
-}
