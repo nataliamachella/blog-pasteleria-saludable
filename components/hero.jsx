5dff88f71b14a41a4b0f33d59ca3169b9636a747
nataliamachella <nataliamachella@gmail.com>
2023-05-25 16:05:07 -0300

    fixed hero

--- components/hero.jsx
+++ components/hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function hero() {
   return (
     <div className="relative w-full h-[400px] bg-[url('/images/bg_home.png')]">
-      <div className="absolute text-center top-[60%] md:top-[35%] md:left-[25%] text-4xl font-bold  md:text-5xl fontHero  text-[#643E17]">
+      <div className="absolute text-center top-[40%] md:top-[35%] md:left-[25%] text-4xl font-bold  md:text-5xl fontHero  text-[#643E17]">
         Recetas de Pastelería Saludable
       </div>
     </div>
